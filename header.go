bf67adc48f18f9c747ccc53057d2ec709337aaeb
Yoshiyuki Koyanagi <moutend@gmail.com>
2017-11-05 18:42:57 +0900

    Add

---
+++ header.go
@@ -0,0 +1,70 @@
+package midi
+
+import (
+	"bytes"
+	"encoding/binary"
+	"fmt"
+	"io"
+)
+
+// Header represents MIDI header. It contains format type, number of tracks and time division.
+type Header struct {
+	formatType   uint16
+	tracks       uint16
+	timeDivision *TimeDivision
+}
+
+// FormatType returns MIDI format type.
+func (h *Header) FormatType() int {
+	return int(h.formatType)
+}
+
+// Tracks returns number of tracks.
+func (h *Header) Tracks() int {
+	return int(h.tracks)
+}
+
+// TimeDivision returns time division.
+func (h *Header) TimeDivision() *TimeDivision {
+	if h.timeDivision == nil {
+		h.timeDivision = &TimeDivision{}
+	}
+	return h.timeDivision
+}
+
+// String returns string representation of the Header.
+// The returned string is meant for debugging.
+func (h *Header) String() string {
+	return fmt.Sprintf("FormatType: %d Tracks: %d TimeDivision: %v", h.formatType, h.tracks, h.TimeDivision().String())
+}
+
+func (h *Header) serialize() []byte {
+	data := bytes.NewBuffer([]byte{})
+
+	binary.Write(data, binary.BigEndian, MThd)
+	binary.Write(data, binary.BigEndian, uint32(6))
+	binary.Write(data, binary.BigEndian, h.formatType)
+	binary.Write(data, binary.BigEndian, h.tracks)
+	binary.Write(data, binary.BigEndian, h.timeDivision.value)
+
+	return data.Bytes()
+}
+
+func parseHeader(stream []byte) (*Header, error) {
+	var chunkId ChunkId
+	var timeDivision uint16
+
+	header := &Header{}
+	data := bytes.NewReader(stream)
+	binary.Read(io.NewSectionReader(data, 0, 4), binary.BigEndian, &chunkId)
+	if chunkId != MThd {
+		return nil, fmt.Errorf("midi: invalid chunk ID for header: %x", chunkId)
+	}
+
+	binary.Read(io.NewSectionReader(data, 8, 2), binary.BigEndian, &header.formatType)
+	binary.Read(io.NewSectionReader(data, 10, 2), binary.BigEndian, &header.tracks)
+	binary.Read(io.NewSectionReader(data, 12, 2), binary.BigEndian, &timeDivision)
+	header.TimeDivision().Set(timeDivision)
+
+	return header, nil
+}
---
+++ header_test.go
@@ -0,0 +1,62 @@
+package midi
+
+import (
+	"bytes"
+	"encoding/binary"
+	"io/ioutil"
+	"path/filepath"
+	"testing"
+)
+
+func TestHeaderSerialize(t *testing.T) {
+	td := &TimeDivision{
+		value: 480,
+	}
+	h := &Header{
+		formatType:   1,
+		tracks:       18,
+		timeDivision: td,
+	}
+	data := h.serialize()
+	// Header Length of the header is always 14 bytes.
+	if len(data) != 14 {
+		t.Fatalf("expected: 14 actual: %v", len(data))
+	}
+
+	buf := bytes.NewBuffer([]byte{})
+	binary.Write(buf, binary.BigEndian, []byte("MThd"))
+	binary.Write(buf, binary.BigEndian, uint32(6))
+	binary.Write(buf, binary.BigEndian, uint16(1))
+	binary.Write(buf, binary.BigEndian, uint16(18))
+	binary.Write(buf, binary.BigEndian, uint16(480))
+
+	for i, b := range buf.Bytes() {
+		if data[i] != b {
+			t.Fatalf("expected: data[%v] = %v actual: data[%v] = %v", i, b, i, data[i])
+		}
+	}
+}
+
+func TestParseHeader(t *testing.T) {
+	pathToMid := filepath.Join("testdata", "vegetable_valley.mid")
+	file, err := ioutil.ReadFile(pathToMid)
+	if err != nil {
+		t.Fatal(err)
+	}
+	header, err := parseHeader(file)
+	if err != nil {
+		t.Fatal(err)
+	}
+	data := header.serialize()
+
+	// Header Length of the header is always 14 bytes.
+	if len(data) != 14 {
+		t.Fatalf("expected: 14 actual: %v", len(data))
+	}
+
+	for i, b := range data {
+		if file[i] != b {
+			t.Fatalf("expected: data[%v] = %v actual: data[%v] = %v", i, file[i], i, b)
+		}
+	}
+}
