7096ac3c8b64dae08173ae717c8369c338b58d17
Yoshiyuki Koyanagi <moutend@gmail.com>
2017-11-09 08:30:51 +0900

    Update

--- constants.go
+++ constants.go
@@ -1,4 +1,35 @@
+// go: generate stringer -type=Note
 package midi
+
+const (
+	NoteOff           = 0x80
+	NoteOn            = 0x90
+	NoteAfterTouch    = 0xa0
+	Controller        = 0xb0
+	ProgramChange     = 0xc0
+	ChannelAfterTouch = 0xd0
+	PitchBend         = 0xe0
+	SystemExclusive   = 0xf0
+	EndOfNormalSysEx  = 0xf7
+)
+
+const (
+	Text                = 0x01
+	CopyrightNotice     = 0x02
+	SequenceOrTrackName = 0x03
+	InstrumentName      = 0x04
+	Lyrics              = 0x05
+	Marker              = 0x06
+	CuePoint            = 0x07
+	MIDIPortPrefix      = 0x20
+	MIDIChannelPrefix   = 0x21
+	SetTempo            = 0x51
+	SMPTEOffset         = 0x54
+	TimeSignature       = 0x58
+	KeySignature        = 0x59
+	SequencerSpecific   = 0x7f
+	EndOfTrack          = 0x2f
+)
 
 // Note
 type Note byte
