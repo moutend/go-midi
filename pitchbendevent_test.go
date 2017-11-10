35a4618a4b37d066227dd26a316dedce2c3fb847
Yoshiyuki Koyanagi <moutend@gmail.com>
2017-11-10 09:33:46 +0900

    Add tests for PitchBendEvent

--- pitchbendevent_test.go
+++ pitchbendevent_test.go
@@ -42,3 +42,64 @@ func TestPitchBendEvent_Serialize(t *testing.T) {
 		}
 	}
 }
+
+func TestPitchBendEvent_SetChannel(t *testing.T) {
+	event := &PitchBendEvent{}
+
+	err := event.SetChannel(0x10)
+	if err == nil {
+		t.Fatalf("err must not be nil")
+	}
+	err = event.SetChannel(0x0f)
+	if err != nil {
+		t.Fatal(err)
+	}
+}
+
+func TestPitchBendEvent_Channel(t *testing.T) {
+	event := &PitchBendEvent{channel: 1}
+
+	expected := uint8(1)
+	actual := event.Channel()
+
+	if expected != actual {
+		t.Fatalf("expected: %v actual: %v", expected, actual)
+	}
+}
+
+func TestPitchBendEvent_SetPitch(t *testing.T) {
+	event := &PitchBendEvent{}
+
+	err := event.SetPitch(0x4fff)
+	if err == nil {
+		t.Fatalf("err must not be nil")
+	}
+	err = event.SetPitch(0x3fff)
+	if err != nil {
+		t.Fatal(err)
+	}
+}
+
+func TestPitchBendEvent_Pitch(t *testing.T) {
+	event := &PitchBendEvent{pitch: 1234}
+
+	expected := uint16(1234)
+	actual := event.Pitch()
+
+	if expected != actual {
+		t.Fatalf("expected: %v actual: %v", expected, actual)
+	}
+}
+
+func TestNewPitchBendEvent(t *testing.T) {
+	event, err := NewPitchBendEvent(nil, 1, 1234)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if event.channel != 1 {
+		t.Fatalf("expected: 1 actual: %v", event.channel)
+	}
+	if event.pitch != 1234 {
+		t.Fatalf("expected: 1234 actual: %v", event.pitch)
+	}
+}
