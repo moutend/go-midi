1e030074c9dce6ad011a7fc5dd73c7405fe1388b
Yoshiyuki Koyanagi <moutend@gmail.com>
2017-11-11 15:03:00 +0900

    Chore

--- event_test.go
+++ event_test.go
@@ -5,13 +5,13 @@ import (
 )
 
 func TestParseEvent(t *testing.T) {
-	stream := []byte{0x00, 0xff, 0x02, 0x11, 0x6f, 0x70, 0x79, 0x72, 0x69, 0x67, 0x68, 0x74, 0x20, 0x28, 0x43, 0x29, 0x20, 0x32, 0x30, 0x31, 0x37}
+	stream := []byte{0x00, 0xff, 0x02, 0x12, 0x43, 0x6f, 0x70, 0x79, 0x72, 0x69, 0x67, 0x68, 0x74, 0x20, 0x28, 0x43, 0x29, 0x20, 0x32, 0x30, 0x31, 0x37}
 	event, sizeOfEvent, err := parseEvent(stream)
 	if err != nil {
 		t.Fatal(err)
 	}
-	if sizeOfEvent != 21 {
-		t.Fatalf("expected: size of event = 21, actual: size of event = %v", sizeOfEvent)
+	if sizeOfEvent != 22 {
+		t.Fatalf("expected: size of event = 22, actual: size of event = %v", sizeOfEvent)
 	}
 	if len(event.DeltaTime().Quantity().Value()) != 1 {
 		t.Fatalf("expected: len(event.deltaTime.Quantity().Value()) = 1 actual: len(event.deltaTime.Quantity().Value()) = %v", len(event.DeltaTime().Quantity().Value()))
