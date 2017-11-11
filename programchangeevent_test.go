f8ed83c7db95ffcda9137abcb0259af0e6c93ac6
Yoshiyuki Koyanagi <moutend@gmail.com>
2017-11-11 10:27:09 +0900

    Update tests

--- programchangeevent_test.go
+++ programchangeevent_test.go
@@ -42,3 +42,64 @@ func TestProgramChangeEvent_Serialize(t *testing.T) {
 		}
 	}
 }
+
+func TestProgramChangeEvent_SetChannel(t *testing.T) {
+	event := &ProgramChangeEvent{}
+
+	err := event.SetChannel(0x10)
+	if err == nil {
+		t.Fatalf("err must not be nil")
+	}
+	err = event.SetChannel(0x0f)
+	if err != nil {
+		t.Fatal(err)
+	}
+}
+
+func TestProgramChangeEvent_Channel(t *testing.T) {
+	event := &ProgramChangeEvent{channel: 1}
+
+	expected := uint8(1)
+	actual := event.Channel()
+
+	if expected != actual {
+		t.Fatalf("expected: %v actual: %v", expected, actual)
+	}
+}
+
+func TestProgramChangeEvent_SetProgram(t *testing.T) {
+	event := &ProgramChangeEvent{}
+
+	err := event.SetProgram(0x80)
+	if err == nil {
+		t.Fatalf("err must not be nil")
+	}
+	err = event.SetProgram(0x7f)
+	if err != nil {
+		t.Fatal(err)
+	}
+}
+
+func TestProgramChangeEvent_Program(t *testing.T) {
+	event := &ProgramChangeEvent{program: 1}
+
+	expected := uint8(1)
+	actual := event.Program()
+
+	if expected != actual {
+		t.Fatalf("expected: %v actual: %v", expected, actual)
+	}
+}
+
+func TestNewProgramChangeEvent(t *testing.T) {
+	event, err := NewProgramChangeEvent(nil, 1, 123)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if event.channel != 1 {
+		t.Fatalf("expected: 1 actual: %v", event.channel)
+	}
+	if event.program != 123 {
+		t.Fatalf("expected: 123 actual: %v", event.program)
+	}
+}
