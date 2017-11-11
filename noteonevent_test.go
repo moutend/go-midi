e6f36f72113fba69cd7c24470159b85452550ba4
Yoshiyuki Koyanagi <moutend@gmail.com>
2017-11-11 10:25:52 +0900

    Update tests

--- noteonevent_test.go
+++ noteonevent_test.go
@@ -42,3 +42,92 @@ func TestNoteOnEvent_Serialize(t *testing.T) {
 		}
 	}
 }
+
+func TestNoteOnEvent_SetChannel(t *testing.T) {
+	event := &NoteOnEvent{}
+
+	err := event.SetChannel(0x10)
+	if err == nil {
+		t.Fatalf("err must not be nil")
+	}
+	err = event.SetChannel(0x0f)
+	if err != nil {
+		t.Fatal(err)
+	}
+}
+
+func TestNoteOnEvent_Channel(t *testing.T) {
+	event := &NoteOnEvent{channel: 1}
+
+	expected := uint8(1)
+	actual := event.Channel()
+
+	if expected != actual {
+		t.Fatalf("expected: %v actual: %v", expected, actual)
+	}
+}
+
+func TestNoteOnEvent_SetNote(t *testing.T) {
+	event := &NoteOnEvent{}
+
+	err := event.SetNote(0x80)
+	if err == nil {
+		t.Fatalf("err must not be nil")
+	}
+	err = event.SetNote(0x7f)
+	if err != nil {
+		t.Fatal(err)
+	}
+}
+
+func TestNoteOnEvent_Note(t *testing.T) {
+	event := &NoteOnEvent{note: C3}
+
+	expected := C3
+	actual := event.Note()
+
+	if expected != actual {
+		t.Fatalf("expected: %v actual: %v", expected, actual)
+	}
+}
+
+func TestNoteOnEvent_SetVelocity(t *testing.T) {
+	event := &NoteOnEvent{}
+
+	err := event.SetVelocity(0x80)
+	if err == nil {
+		t.Fatalf("err must not be nil")
+	}
+	err = event.SetVelocity(0x7f)
+	if err != nil {
+		t.Fatal(err)
+	}
+}
+
+func TestNoteOnEvent_Velocity(t *testing.T) {
+	event := &NoteOnEvent{velocity: 1}
+
+	expected := uint8(1)
+	actual := event.Velocity()
+
+	if expected != actual {
+		t.Fatalf("expected: %v actual: %v", expected, actual)
+	}
+}
+
+func TestNewNoteOnEvent(t *testing.T) {
+	event, err := NewNoteOnEvent(nil, 1, C3, 123)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if event.channel != 1 {
+		t.Fatalf("expected: 1 actual: %v", event.channel)
+	}
+	if event.note != C3 {
+		t.Fatalf("expected: C3 actual: %v", event.note)
+	}
+	if event.velocity != 123 {
+		t.Fatalf("expected: 123 actual: %v", event.velocity)
+	}
+
+}
