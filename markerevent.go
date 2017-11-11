85c0b6fa664b41ce1ed396a4323316d67225a22a
Yoshiyuki Koyanagi <moutend@gmail.com>
2017-11-11 10:20:30 +0900

    Update tests

--- markerevent.go
+++ markerevent.go
@@ -2,15 +2,65 @@ package midi
 
 import "fmt"
 
+// MarkerEvent corresponds to marker meta event.
 type MarkerEvent struct {
 	deltaTime *DeltaTime
 	text      []byte
 }
 
+// DeltaTime returns delta time of marker event.
 func (e *MarkerEvent) DeltaTime() *DeltaTime {
+	if e.deltaTime == nil {
+		e.deltaTime = &DeltaTime{}
+	}
 	return e.deltaTime
 }
 
+// String returns string representation of marker event.
 func (e *MarkerEvent) String() string {
 	return fmt.Sprintf("&MarkerEvent{text: \"%v\"}", string(e.text))
+}
+
+// Serialize serializes marker event.
+func (e *MarkerEvent) Serialize() []byte {
+	bs := []byte{}
+	bs = append(bs, e.DeltaTime().Quantity().Value()...)
+	bs = append(bs, Meta, Marker)
+	bs = append(bs, byte(len(e.Text())))
+	bs = append(bs, e.Text()...)
+
+	return bs
+}
+
+// SetText sets text.
+func (e *MarkerEvent) SetText(text []byte) error {
+	if len(text) > 127 {
+		return fmt.Errorf("midi: maximum length of text is 127 bytes")
+	}
+	e.text = text
+
+	return nil
+}
+
+// Text returns text.
+func (e *MarkerEvent) Text() []byte {
+	if e.text == nil {
+		e.text = []byte{}
+	}
+
+	return e.text
+}
+
+// NewMarkerEvent returns MarkerEvent with the given parameter.
+func NewMarkerEvent(deltaTime *DeltaTime, text []byte) (*MarkerEvent, error) {
+	var err error
+
+	event := &MarkerEvent{}
+	event.deltaTime = deltaTime
+
+	err = event.SetText(text)
+	if err != nil {
+		return nil, err
+	}
+	return event, nil
 }
---
+++ markerevent_test.go
@@ -0,0 +1,90 @@
+package midi
+
+import "testing"
+
+func TestMarkerEvent_DeltaTime(t *testing.T) {
+	event := &MarkerEvent{}
+	dt := event.DeltaTime()
+	if dt == nil {
+		t.Fatal("DeltaTime() don't return nil")
+	}
+}
+
+func TestMarkerEvent_String(t *testing.T) {
+	event, err := NewMarkerEvent(nil, []byte("text"))
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	expected := "&MarkerEvent{text: \"text\"}"
+	actual := event.String()
+	if expected != actual {
+		t.Fatalf("expected: %v actual: %v", expected, actual)
+	}
+}
+
+func TestMarkerEvent_Serialize(t *testing.T) {
+	event, err := NewMarkerEvent(nil, []byte("text"))
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	expected := []byte{0x00, 0xff, 0x06, 0x04, 0x74, 0x65, 0x78, 0x74}
+	actual := event.Serialize()
+
+	if len(expected) != len(actual) {
+		t.Fatalf("expected: %v bytes actual: %v bytes", len(expected), len(actual))
+	}
+	for i, e := range expected {
+		a := actual[i]
+		if e != a {
+			t.Fatalf("expected[%v] = 0x%x actual[%v] = 0x%x", i, e, i, a)
+		}
+	}
+}
+
+func TestMarkerEvent_SetText(t *testing.T) {
+	event := &MarkerEvent{}
+	text := make([]byte, 128)
+
+	err := event.SetText(text)
+	if err == nil {
+		t.Fatalf("err must not be nil")
+	}
+
+	text = make([]byte, 127)
+	err = event.SetText(text)
+	if err != nil {
+		t.Fatal(err)
+	}
+}
+
+func TestMarkerEvent_Text(t *testing.T) {
+	event := &MarkerEvent{text: []byte("text")}
+
+	expected := "text"
+	actual := string(event.Text())
+
+	if expected != actual {
+		t.Fatalf("expected: %v actual: %v", expected, actual)
+	}
+}
+
+func TestNewMarkerEvent(t *testing.T) {
+	event, err := NewMarkerEvent(nil, []byte("text"))
+	if err != nil {
+		t.Fatal(err)
+	}
+	expected := []byte("text")
+	actual := event.text
+
+	if len(expected) != len(actual) {
+		t.Fatalf("expected: %v bytes actual: %v bytes", len(expected), len(actual))
+	}
+	for i, e := range expected {
+		a := actual[i]
+		if e != a {
+			t.Fatalf("expected[%v] = 0x%x actual[%v] = 0x%x", i, e, i, a)
+		}
+	}
+}
