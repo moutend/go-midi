6b6b423217e39f66a5822e8d89e7b611fd003b11
Yoshiyuki Koyanagi <moutend@gmail.com>
2017-11-11 10:28:46 +0900

    Update tests

--- endoftrackevent_test.go
+++ endoftrackevent_test.go
@@ -2,6 +2,27 @@ package midi
 
 import "testing"
 
+func TestEndOfTrackEvent_DeltaTime(t *testing.T) {
+	event := &EndOfTrackEvent{}
+	dt := event.DeltaTime()
+	if dt == nil {
+		t.Fatal("DeltaTime() don't return nil")
+	}
+}
+
+func TestEndOfTrackEvent_String(t *testing.T) {
+	event, err := NewEndOfTrackEvent(nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	expected := "&EndOfTrackEvent{}"
+	actual := event.String()
+	if expected != actual {
+		t.Fatalf("expected: %v actual: %v", expected, actual)
+	}
+}
+
 func TestEndOfTrackEvent_Serialize(t *testing.T) {
 	event := &EndOfTrackEvent{}
 	expected := []byte{0x00, 0xff, 0x2f, 0x00}
@@ -9,5 +30,12 @@ func TestEndOfTrackEvent_Serialize(t *testing.T) {
 
 	if len(expected) != len(actual) {
 		t.Fatalf("expected: %v bytes actual: %v bytes", len(expected), len(actual))
+	}
+}
+
+func TestNewEndOfTrackEvent(t *testing.T) {
+	_, err := NewEndOfTrackEvent(nil)
+	if err != nil {
+		t.Fatalf("err must be always nil")
 	}
 }
