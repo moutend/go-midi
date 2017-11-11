b34258a6f532ebba064f746650eb12d2657cebe6
Yoshiyuki Koyanagi <moutend@gmail.com>
2017-11-11 12:12:07 +0900

    Update tests

--- systemexclusiveevent.go
+++ systemexclusiveevent.go
@@ -2,15 +2,67 @@ package midi
 
 import "fmt"
 
+// SystemExclusiveEvent corresponds to system exclusive meta event.
 type SystemExclusiveEvent struct {
 	deltaTime *DeltaTime
 	data      []byte
 }
 
+// DeltaTime returns delta time of system exclusive event.
 func (e *SystemExclusiveEvent) DeltaTime() *DeltaTime {
+	if e.deltaTime == nil {
+		e.deltaTime = &DeltaTime{}
+	}
 	return e.deltaTime
 }
 
+// String returns string representation of system exclusive event.
 func (e *SystemExclusiveEvent) String() string {
-	return fmt.Sprintf("&SystemExclusiveEvent{data: %v bytes}", len(e.data))
+	return fmt.Sprintf("&SystemExclusiveEvent{data: %v bytes}", len(e.Data()))
+}
+
+// Serialize serializes system exclusive event.
+func (e *SystemExclusiveEvent) Serialize() []byte {
+	bs := []byte{}
+	bs = append(bs, e.DeltaTime().Quantity().Value()...)
+	bs = append(bs, SystemExclusive)
+	q := &Quantity{}
+	q.SetUint32(uint32(len(e.Data())))
+	bs = append(bs, q.Serialize()...)
+	bs = append(bs, e.Data()...)
+
+	return bs
+}
+
+// SetData sets data.
+func (e *SystemExclusiveEvent) SetData(data []byte) error {
+	if len(data) > 0xfffffff {
+		return fmt.Errorf("midi: maximum size of data is 256 MB")
+	}
+	e.data = data
+
+	return nil
+}
+
+// Data returns data.
+func (e *SystemExclusiveEvent) Data() []byte {
+	if e.data == nil {
+		e.data = []byte{}
+	}
+
+	return e.data
+}
+
+// NewSystemExclusiveEvent returns SystemExclusiveEvent with the given parameter.
+func NewSystemExclusiveEvent(deltaTime *DeltaTime, data []byte) (*SystemExclusiveEvent, error) {
+	var err error
+
+	event := &SystemExclusiveEvent{}
+	event.deltaTime = deltaTime
+
+	err = event.SetData(data)
+	if err != nil {
+		return nil, err
+	}
+	return event, nil
 }
---
+++ systemexclusiveevent_test.go
@@ -0,0 +1,100 @@
+package midi
+
+import "testing"
+
+func TestSystemExclusiveEvent_DeltaTime(t *testing.T) {
+	event := &SystemExclusiveEvent{}
+	dt := event.DeltaTime()
+	if dt == nil {
+		t.Fatal("DeltaTime() don't return nil")
+	}
+}
+
+func TestSystemExclusiveEvent_String(t *testing.T) {
+	event, err := NewSystemExclusiveEvent(nil, []byte{0x01, 0x02, 0x03, 0x04})
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	expected := "&SystemExclusiveEvent{data: 4 bytes}"
+	actual := event.String()
+	if expected != actual {
+		t.Fatalf("expected: %v actual: %v", expected, actual)
+	}
+}
+
+func TestSystemExclusiveEvent_Serialize(t *testing.T) {
+	event, err := NewSystemExclusiveEvent(nil, []byte{0x11, 0x12, 0x13, 0x14})
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	expected := []byte{0x00, 0xf0, 0x04, 0x11, 0x12, 0x13, 0x14}
+	actual := event.Serialize()
+
+	if len(expected) != len(actual) {
+		t.Fatalf("expected: %v bytes actual: %v bytes", len(expected), len(actual))
+	}
+	for i, e := range expected {
+		a := actual[i]
+		if e != a {
+			t.Fatalf("expected[%v] = 0x%x actual[%v] = 0x%x", i, e, i, a)
+		}
+	}
+}
+
+func TestSystemExclusiveEvent_SetData(t *testing.T) {
+	event := &SystemExclusiveEvent{}
+	data := make([]byte, 0x10000000)
+
+	err := event.SetData(data)
+	if err == nil {
+		t.Fatalf("err must not be nil")
+	}
+
+	data = make([]byte, 100)
+	err = event.SetData(data)
+	if err != nil {
+		t.Fatal(err)
+	}
+}
+
+func TestSystemExclusiveEvent_Data(t *testing.T) {
+	event := &SystemExclusiveEvent{}
+	if event.Data() == nil {
+		t.Fatalf("Data() must return empty slice")
+	}
+
+	event.data = []byte{0x11, 0x12, 0x13, 0x14}
+	expected := []byte{0x11, 0x12, 0x13, 0x14}
+	actual := event.Data()
+
+	if len(expected) != len(actual) {
+		t.Fatalf("expected: %v bytes actual: %v bytes", len(expected), len(actual))
+	}
+	for i, e := range expected {
+		a := actual[i]
+		if e != a {
+			t.Fatalf("expected[%v] = 0x%x actual[%v] = 0x%x", i, e, i, a)
+		}
+	}
+}
+
+func TestNewSystemExclusiveEvent(t *testing.T) {
+	event, err := NewSystemExclusiveEvent(nil, []byte{0x11, 0x12, 0x13, 0x14})
+	if err != nil {
+		t.Fatal(err)
+	}
+	expected := []byte{0x11, 0x12, 0x13, 0x14}
+	actual := event.data
+
+	if len(expected) != len(actual) {
+		t.Fatalf("expected: %v bytes actual: %v bytes", len(expected), len(actual))
+	}
+	for i, e := range expected {
+		a := actual[i]
+		if e != a {
+			t.Fatalf("expected[%v] = 0x%x actual[%v] = 0x%x", i, e, i, a)
+		}
+	}
+}
