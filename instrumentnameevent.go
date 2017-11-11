e7dfd40a6ad4d70ba1e5ef0d41e3b07e08476fa1
Yoshiyuki Koyanagi <moutend@gmail.com>
2017-11-11 12:20:31 +0900

    Update tests

--- instrumentnameevent.go
+++ instrumentnameevent.go
@@ -2,15 +2,65 @@ package midi
 
 import "fmt"
 
+// InstrumentNameEvent corresponds to instrument name meta event.
 type InstrumentNameEvent struct {
 	deltaTime *DeltaTime
 	text      []byte
 }
 
+// DeltaTime returns delta time of instrument name event.
 func (e *InstrumentNameEvent) DeltaTime() *DeltaTime {
+	if e.deltaTime == nil {
+		e.deltaTime = &DeltaTime{}
+	}
 	return e.deltaTime
 }
 
+// String returns string representation of instrument name event.
 func (e *InstrumentNameEvent) String() string {
-	return fmt.Sprintf("&InstrumentNameEvent{}")
+	return fmt.Sprintf("&InstrumentNameEvent{text: \"%v\"}", string(e.text))
+}
+
+// Serialize serializes instrument name event.
+func (e *InstrumentNameEvent) Serialize() []byte {
+	bs := []byte{}
+	bs = append(bs, e.DeltaTime().Quantity().Value()...)
+	bs = append(bs, Meta, InstrumentName)
+	bs = append(bs, byte(len(e.Text())))
+	bs = append(bs, e.Text()...)
+
+	return bs
+}
+
+// SetText sets text.
+func (e *InstrumentNameEvent) SetText(text []byte) error {
+	if len(text) > 127 {
+		return fmt.Errorf("midi: maximum length of text is 127 bytes")
+	}
+	e.text = text
+
+	return nil
+}
+
+// Text returns text.
+func (e *InstrumentNameEvent) Text() []byte {
+	if e.text == nil {
+		e.text = []byte{}
+	}
+
+	return e.text
+}
+
+// NewInstrumentNameEvent returns InstrumentNameEvent with the given parameter.
+func NewInstrumentNameEvent(deltaTime *DeltaTime, text []byte) (*InstrumentNameEvent, error) {
+	var err error
+
+	event := &InstrumentNameEvent{}
+	event.deltaTime = deltaTime
+
+	err = event.SetText(text)
+	if err != nil {
+		return nil, err
+	}
+	return event, nil
 }
---
+++ instrumentnameevent_test.go
@@ -0,0 +1,90 @@
+package midi
+
+import "testing"
+
+func TestInstrumentNameEvent_DeltaTime(t *testing.T) {
+	event := &InstrumentNameEvent{}
+	dt := event.DeltaTime()
+	if dt == nil {
+		t.Fatal("DeltaTime() don't return nil")
+	}
+}
+
+func TestInstrumentNameEvent_String(t *testing.T) {
+	event, err := NewInstrumentNameEvent(nil, []byte("text"))
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	expected := "&InstrumentNameEvent{text: \"text\"}"
+	actual := event.String()
+	if expected != actual {
+		t.Fatalf("expected: %v actual: %v", expected, actual)
+	}
+}
+
+func TestInstrumentNameEvent_Serialize(t *testing.T) {
+	event, err := NewInstrumentNameEvent(nil, []byte("text"))
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	expected := []byte{0x00, 0xff, 0x04, 0x04, 0x74, 0x65, 0x78, 0x74}
+	actual := event.Serialize()
+
+	if len(expected) != len(actual) {
+		t.Fatalf("expected: %v bytes actual: %v bytes", len(expected), len(actual))
+	}
+	for i, e := range expected {
+		a := actual[i]
+		if e != a {
+			t.Fatalf("expected[%v] = 0x%x actual[%v] = 0x%x", i, e, i, a)
+		}
+	}
+}
+
+func TestInstrumentNameEvent_SetText(t *testing.T) {
+	event := &InstrumentNameEvent{}
+	text := make([]byte, 128)
+
+	err := event.SetText(text)
+	if err == nil {
+		t.Fatalf("err must not be nil")
+	}
+
+	text = make([]byte, 127)
+	err = event.SetText(text)
+	if err != nil {
+		t.Fatal(err)
+	}
+}
+
+func TestInstrumentNameEvent_Text(t *testing.T) {
+	event := &InstrumentNameEvent{text: []byte("text")}
+
+	expected := "text"
+	actual := string(event.Text())
+
+	if expected != actual {
+		t.Fatalf("expected: %v actual: %v", expected, actual)
+	}
+}
+
+func TestNewInstrumentNameEvent(t *testing.T) {
+	event, err := NewInstrumentNameEvent(nil, []byte("text"))
+	if err != nil {
+		t.Fatal(err)
+	}
+	expected := []byte("text")
+	actual := event.text
+
+	if len(expected) != len(actual) {
+		t.Fatalf("expected: %v bytes actual: %v bytes", len(expected), len(actual))
+	}
+	for i, e := range expected {
+		a := actual[i]
+		if e != a {
+			t.Fatalf("expected[%v] = 0x%x actual[%v] = 0x%x", i, e, i, a)
+		}
+	}
+}
