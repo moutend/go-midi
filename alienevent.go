8153fcd1bc0e694ab9eb6efe26eb163c8dd7a07d
Yoshiyuki Koyanagi <moutend@gmail.com>
2017-11-12 07:45:36 +0900

    Update tests

--- alienevent.go
+++ alienevent.go
@@ -2,16 +2,84 @@ package midi
 
 import "fmt"
 
+// Alien represents unknown meta event.
 type AlienEvent struct {
 	deltaTime     *DeltaTime
-	metaEventType byte
+	metaEventType uint8
 	data          []byte
 }
 
+// DeltaTime returns delta time.
 func (e *AlienEvent) DeltaTime() *DeltaTime {
+	if e.deltaTime == nil {
+		e.deltaTime = &DeltaTime{}
+	}
 	return e.deltaTime
 }
 
+// String returns string representation of alien event.
 func (e *AlienEvent) String() string {
-	return fmt.Sprintf("&AlienEvent{metaEventType: 0x%x data: %v bytes}", e.metaEventType, len(e.data))
+	return fmt.Sprintf("&AlienEvent{metaEventType: 0x%x, data: %v bytes}", e.metaEventType, len(e.Data()))
+}
+
+// Serialize serializes alien event.
+func (e *AlienEvent) Serialize() []byte {
+	bs := []byte{}
+	bs = append(bs, e.DeltaTime().Quantity().Value()...)
+	bs = append(bs, Meta, e.metaEventType)
+
+	q := &Quantity{}
+	q.SetUint32(uint32(len(e.Data())))
+	bs = append(bs, q.Value()...)
+	bs = append(bs, e.Data()...)
+
+	return bs
+}
+
+// SetMetaEventType sets meta event type.
+func (e *AlienEvent) SetMetaEventType(metaEventType uint8) error {
+	e.metaEventType = metaEventType
+
+	return nil
+}
+
+// MetaEventType returns meta event type.
+func (e *AlienEvent) MetaEventType() uint8 {
+	return e.metaEventType
+}
+
+// SetData sets data.
+func (e *AlienEvent) SetData(data []byte) error {
+	if len(data) > 0xfffffff {
+		return fmt.Errorf("midi: maximum length of data is 256 MB")
+	}
+	e.data = data
+
+	return nil
+}
+
+// Data returns data.
+func (e *AlienEvent) Data() []byte {
+	if e.data == nil {
+		e.data = []byte{}
+	}
+	return e.data
+}
+
+// NewAlienEvent returns AlienEvent with the given parameter.
+func NewAlienEvent(deltaTime *DeltaTime, metaEventType uint8, data []byte) (*AlienEvent, error) {
+	var err error
+
+	event := &AlienEvent{}
+	event.deltaTime = deltaTime
+
+	err = event.SetMetaEventType(metaEventType)
+	if err != nil {
+		return nil, err
+	}
+	err = event.SetData(data)
+	if err != nil {
+		return nil, err
+	}
+	return event, nil
 }
---
+++ alienevent_test.go
@@ -0,0 +1,96 @@
+package midi
+
+import "testing"
+
+func TestAlienEvent_DeltaTime(t *testing.T) {
+	event := &AlienEvent{}
+	dt := event.DeltaTime()
+	if dt == nil {
+		t.Fatal("DeltaTime() don't return nil")
+	}
+}
+
+func TestAlienEvent_String(t *testing.T) {
+	event, err := NewAlienEvent(nil, 0xaa, []byte{0x12, 0x34, 0x56})
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	expected := "&AlienEvent{metaEventType: 0xaa, data: 3 bytes}"
+	actual := event.String()
+	if expected != actual {
+		t.Fatalf("expected: %v actual: %v", expected, actual)
+	}
+}
+
+func TestAlienEvent_Serialize(t *testing.T) {
+	event, err := NewAlienEvent(nil, 0xaa, []byte{0x12, 0x34, 0x56})
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	expected := []byte{0x00, 0xff, 0xaa, 0x03, 0x12, 0x34, 0x56}
+	actual := event.Serialize()
+
+	if len(expected) != len(actual) {
+		t.Fatalf("expected: %v bytes actual: %v bytes", len(expected), len(actual))
+	}
+	for i, e := range expected {
+		a := actual[i]
+		if e != a {
+			t.Fatalf("expected[%v] = 0x%x actual[%v] = 0x%x", i, e, i, a)
+		}
+	}
+}
+
+func TestAlienEvent_SetData(t *testing.T) {
+	event := &AlienEvent{}
+	data := make([]byte, 0x10000000)
+
+	err := event.SetData(data)
+	if err == nil {
+		t.Fatalf("err must not be nil")
+	}
+
+	data = make([]byte, 0xfffffff)
+	err = event.SetData(data)
+	if err != nil {
+		t.Fatal(err)
+	}
+}
+
+func TestAlienEvent_Data(t *testing.T) {
+	event := &AlienEvent{data: []byte{0x12, 0x34, 0x56}}
+
+	expected := []byte{0x12, 0x34, 0x56}
+	actual := event.Data()
+
+	if len(expected) != len(actual) {
+		t.Fatalf("expected: %v bytes actual: %v bytes", len(expected), len(actual))
+	}
+	for i, e := range expected {
+		a := actual[i]
+		if e != a {
+			t.Fatalf("expected[%v] = 0x%x actual[%v] = 0x%x", i, e, i, a)
+		}
+	}
+}
+
+func TestNewAlienEvent(t *testing.T) {
+	event, err := NewAlienEvent(nil, 0xaa, []byte{0x12, 0x34, 0x56})
+	if err != nil {
+		t.Fatal(err)
+	}
+	expected := []byte{0x12, 0x34, 0x56}
+	actual := event.data
+
+	if len(expected) != len(actual) {
+		t.Fatalf("expected: %v bytes actual: %v bytes", len(expected), len(actual))
+	}
+	for i, e := range expected {
+		a := actual[i]
+		if e != a {
+			t.Fatalf("expected[%v] = 0x%x actual[%v] = 0x%x", i, e, i, a)
+		}
+	}
+}
