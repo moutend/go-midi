a44b614a0e40629ef580294124c806c8ea8c96e5
Yoshiyuki Koyanagi <moutend@gmail.com>
2017-11-12 09:28:22 +0900

    Update tests

--- lyricsevent_test.go
+++ lyricsevent_test.go
@@ -45,14 +45,14 @@ func TestLyricsEvent_Serialize(t *testing.T) {
 
 func TestLyricsEvent_SetText(t *testing.T) {
 	event := &LyricsEvent{}
-	text := make([]byte, 128)
+	text := make([]byte, 0x10000000)
 
 	err := event.SetText(text)
 	if err == nil {
 		t.Fatalf("err must not be nil")
 	}
 
-	text = make([]byte, 127)
+	text = make([]byte, 0xfffffff)
 	err = event.SetText(text)
 	if err != nil {
 		t.Fatal(err)
