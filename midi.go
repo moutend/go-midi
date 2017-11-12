fcbfe0c12091494fdad18b12cc49e2edeeb6ed1d
Yoshiyuki Koyanagi <moutend@gmail.com>
2017-11-12 12:48:02 +0900

    Update tests

--- midi.go
+++ midi.go
@@ -1,23 +1,38 @@
 package midi
 
+// MIDI represents standard MIDI data.
 type MIDI struct {
 	Header *Header
 	Tracks []*Track
 }
 
+// Serialize serializes MIDI data.
+func (m *MIDI) Serialize() []byte {
+	stream := []byte{}
+	stream = append(stream, m.Header.Serialize()...)
+	for _, track := range m.Tracks {
+		stream = append(stream, track.Serialize()...)
+	}
+
+	return stream
+}
+
+// Parse parses standard MIDI (*.mid) stream.
 func Parse(stream []byte) (*MIDI, error) {
-	midi := &MIDI{}
 	header, err := parseHeader(stream)
 	if err != nil {
 		return nil, err
 	}
-	tracks, err := parseTracks(stream, int(header.tracks))
+
+	tracks, err := parseTracks(stream[14:], int(header.tracks))
 	if err != nil {
 		return nil, err
 	}
 
-	midi.Header = header
-	midi.Tracks = tracks
+	midi := &MIDI{
+		Header: header,
+		Tracks: tracks,
+	}
 
 	return midi, nil
 }
