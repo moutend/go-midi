68128d7019c7eaff24aa9696a4fc344dd98dfa5a
Yoshiyuki Koyanagi <moutend@gmail.com>
2017-11-14 08:25:13 +0900

    Use all .mid files in testdata

--- midi_test.go
+++ midi_test.go
@@ -2,44 +2,45 @@ package midi
 
 import (
 	"io/ioutil"
-	"path/filepath"
 	"testing"
 )
 
 func TestMIDI_Serialize(t *testing.T) {
-	pathToMid := filepath.Join("testdata", "vegetable_valley.mid")
-	file, err := ioutil.ReadFile(pathToMid)
-	if err != nil {
-		t.Fatal(err)
-	}
+	for _, pathToMid := range pathsToMid {
+		file, err := ioutil.ReadFile(pathToMid)
+		if err != nil {
+			t.Fatal(err)
+		}
 
-	m, err := Parse(file)
-	if err != nil {
-		t.Fatal(err)
-	}
+		m, err := Parse(file)
+		if err != nil {
+			t.Fatal(err)
+		}
 
-	expected := file
-	actual := m.Serialize()
+		expected := file
+		actual := m.Serialize()
 
-	if len(expected) != len(actual) {
-		t.Fatalf("expected: %v bytes actual: %v bytes", len(expected), len(actual))
-	}
-	for i, e := range expected {
-		a := actual[i]
-		if e != a {
-			t.Fatalf("expected[%v] = 0x%x actual[%v] = 0x%x", i, e, i, a)
+		if len(expected) != len(actual) {
+			t.Fatalf("expected: %v bytes actual: %v bytes", len(expected), len(actual))
+		}
+		for i, e := range expected {
+			a := actual[i]
+			if e != a {
+				t.Fatalf("expected[%v] = 0x%x actual[%v] = 0x%x", i, e, i, a)
+			}
 		}
 	}
 }
 
 func TestParse(t *testing.T) {
-	pathToMid := filepath.Join("testdata", "vegetable_valley.mid")
-	file, err := ioutil.ReadFile(pathToMid)
-	if err != nil {
-		t.Fatal(err)
-	}
-	_, err = Parse(file)
-	if err != nil {
-		t.Fatal(err)
+	for _, pathToMid := range pathsToMid {
+		file, err := ioutil.ReadFile(pathToMid)
+		if err != nil {
+			t.Fatal(err)
+		}
+		_, err = Parse(file)
+		if err != nil {
+			t.Fatal(err)
+		}
 	}
 }
--- setup_test.go
+++ setup_test.go
@@ -3,17 +3,25 @@ package midi
 import (
 	"os"
 	"path/filepath"
+	"strings"
 	"testing"
 )
 
 var (
-	bigdata   []byte
-	pathToMid string
+	bigdata    []byte
+	pathsToMid []string
 )
+
+func walkFunc(path string, info os.FileInfo, err error) error {
+	if strings.HasSuffix(path, ".mid") {
+		pathsToMid = append(pathsToMid, path)
+	}
+	return nil
+}
 
 func setup() {
 	bigdata = make([]byte, 0x10000000)
-	pathToMid = filepath.Join("testdata", "vegetable_valley.mid")
+	filepath.Walk("testdata", walkFunc)
 }
 
 func TestMain(m *testing.M) {
