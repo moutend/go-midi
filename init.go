3a796682e3ab7e736cce9175fa16df54b3bc306b
Yoshiyuki Koyanagi <moutend@gmail.com>
2017-11-15 09:04:13 +0900

    Add Println and Printf for debugging

--- init.go
+++ init.go
@@ -1,12 +1,35 @@
 package midi
 
 import (
+	"fmt"
 	"io/ioutil"
 	"log"
 )
 
-var logger *log.Logger
+type midiLogger struct {
+	*log.Logger
+	parsedBytes int
+}
+
+func (l *midiLogger) Printf(format string, v ...interface{}) {
+	format = fmt.Sprintf("midi: [%v] %v", l.parsedBytes, format)
+	l.Logger.Printf(format, v...)
+}
+
+func (l *midiLogger) Println(v ...interface{}) {
+	a := make([]interface{}, len(v)+1)
+	a[0] = fmt.Sprintf("midi: [%v]", l.parsedBytes)
+
+	for i := 0; i < len(v); i++ {
+		a[i+1] = v[i]
+	}
+
+	l.Logger.Println(a...)
+}
+
+var logger *midiLogger
 
 func init() {
-	logger = log.New(ioutil.Discard, "discard logging messages", log.LstdFlags)
+	logger = &midiLogger{}
+	logger.Logger = log.New(ioutil.Discard, "discard logging messages", log.LstdFlags)
 }
