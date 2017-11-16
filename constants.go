f0e6a36df949dc2513d1855f5ec74a1fc1b65b46
Yoshiyuki Koyanagi <moutend@gmail.com>
2017-11-16 19:35:47 +0900

    Add constants

--- constants.go
+++ constants.go
@@ -1,6 +1,7 @@
 //go:generate stringer -type=Note -output=note_string.go
 //go:generate stringer -type=GM -output=gm_string.go
 //go:generate stringer -type=Rhythm -output=rhythm_string.go
+//go:generate stringer -type=Control -output=control_string.go
 
 package midi
 
@@ -354,69 +355,71 @@ const (
 	Gunshot
 )
 
+type Control uint8
+
 const (
-	BankSelect                      = 0x00
-	Modulation                      = 0x01
-	BreathController                = 0x02
-	FootController                  = 0x04
-	PortamentoTime                  = 0x05
-	DataEntry                       = 0x06
-	MainVolume                      = 0x07
-	Balance                         = 0x08
-	Pan                             = 0x0a
-	Expression                      = 0x0b
-	EffectControl1                  = 0x0c
-	EffectControl2                  = 0x0d
-	GeneralPurposeController1       = 0x10
-	GeneralPurposeController2       = 0x11
-	GeneralPurposeController3       = 0x12
-	GeneralPurposeController4       = 0x13
-	BankSelectLSB                   = 0x20
-	ModulationLSB                   = 0x21
-	BreathControllerLSB             = 0x22
-	FootControllerLSB               = 0x24
-	PortamentoTimeLSB               = 0x25
-	DataEntryLSB                    = 0x26
-	MainVolumeLSB                   = 0x27
-	BalanceLSB                      = 0x28
-	PanLSB                          = 0x2a
-	ExpressionLSB                   = 0x2b
-	EffectControl1LSB               = 0x2c
-	EffectControl2LSB               = 0x2d
-	GeneralPurposeController1LSB    = 0x30
-	GeneralPurposeController2LSB    = 0x31
-	GeneralPurposeController3LSB    = 0x32
-	GeneralPurposeController4LSB    = 0x33
-	Hold1                           = 0x40
-	PortamentoOnOff                 = 0x41
-	Sostenuto                       = 0x42
-	SoftPedal                       = 0x43
-	LegatoFootswitch                = 0x44
-	Hold2                           = 0x45
-	SoundVariation                  = 0x46
-	HarmonicIntensity               = 0x47
-	ReleaseTime                     = 0x48
-	AttackTime                      = 0x49
-	Brightness                      = 0x4a
-	DecayTime                       = 0x4b
-	VibratoRate                     = 0x4c
-	VibratoDepth                    = 0x4d
-	VibratoDelay                    = 0x4e
-	UndefinedSoundController        = 0x4f
-	GeneralPurposeController5       = 0x50
-	GeneralPurposeController6       = 0x51
-	GeneralPurposeController7       = 0x52
-	GeneralPurposeController8       = 0x53
-	PortamentoControl               = 0x54
-	ReverbSendLevel                 = 0x5b
-	TremoloDepth                    = 0x5c
-	ChorusSendLevel                 = 0x5d
-	CelesteDepth                    = 0x5e
-	PhaserDepth                     = 0x5f
-	DataIncrement                   = 0x60
-	DataDecrement                   = 0x61
-	NonRegisteredParameterNumberLSB = 0x62
-	NonRegisteredParameterNumberMSB = 0x63
-	RegisteredParameterNumberLSB    = 0x64
-	RegisteredParameterNumberMSB    = 0x65
+	BankSelect                      Control = 0x00
+	Modulation                      Control = 0x01
+	BreathController                Control = 0x02
+	FootController                  Control = 0x04
+	PortamentoTime                  Control = 0x05
+	DataEntry                       Control = 0x06
+	MainVolume                      Control = 0x07
+	Balance                         Control = 0x08
+	Pan                             Control = 0x0a
+	Expression                      Control = 0x0b
+	EffectControl1                  Control = 0x0c
+	EffectControl2                  Control = 0x0d
+	GeneralPurposeController1       Control = 0x10
+	GeneralPurposeController2       Control = 0x11
+	GeneralPurposeController3       Control = 0x12
+	GeneralPurposeController4       Control = 0x13
+	BankSelectLSB                   Control = 0x20
+	ModulationLSB                   Control = 0x21
+	BreathControllerLSB             Control = 0x22
+	FootControllerLSB               Control = 0x24
+	PortamentoTimeLSB               Control = 0x25
+	DataEntryLSB                    Control = 0x26
+	MainVolumeLSB                   Control = 0x27
+	BalanceLSB                      Control = 0x28
+	PanLSB                          Control = 0x2a
+	ExpressionLSB                   Control = 0x2b
+	EffectControl1LSB               Control = 0x2c
+	EffectControl2LSB               Control = 0x2d
+	GeneralPurposeController1LSB    Control = 0x30
+	GeneralPurposeController2LSB    Control = 0x31
+	GeneralPurposeController3LSB    Control = 0x32
+	GeneralPurposeController4LSB    Control = 0x33
+	Hold1                           Control = 0x40
+	PortamentoOnOff                 Control = 0x41
+	Sostenuto                       Control = 0x42
+	SoftPedal                       Control = 0x43
+	LegatoFootswitch                Control = 0x44
+	Hold2                           Control = 0x45
+	SoundVariation                  Control = 0x46
+	HarmonicIntensity               Control = 0x47
+	ReleaseTime                     Control = 0x48
+	AttackTime                      Control = 0x49
+	Brightness                      Control = 0x4a
+	DecayTime                       Control = 0x4b
+	VibratoRate                     Control = 0x4c
+	VibratoDepth                    Control = 0x4d
+	VibratoDelay                    Control = 0x4e
+	UndefinedSoundController        Control = 0x4f
+	GeneralPurposeController5       Control = 0x50
+	GeneralPurposeController6       Control = 0x51
+	GeneralPurposeController7       Control = 0x52
+	GeneralPurposeController8       Control = 0x53
+	PortamentoControl               Control = 0x54
+	ReverbSendLevel                 Control = 0x5b
+	TremoloDepth                    Control = 0x5c
+	ChorusSendLevel                 Control = 0x5d
+	CelesteDepth                    Control = 0x5e
+	PhaserDepth                     Control = 0x5f
+	DataIncrement                   Control = 0x60
+	DataDecrement                   Control = 0x61
+	NonRegisteredParameterNumberLSB Control = 0x62
+	NonRegisteredParameterNumberMSB Control = 0x63
+	RegisteredParameterNumberLSB    Control = 0x64
+	RegisteredParameterNumberMSB    Control = 0x65
 )
