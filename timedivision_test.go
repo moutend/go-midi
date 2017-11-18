f18433457af2f473bf4db7b3e79f9d51a2f71a65
Yoshiyuki Koyanagi <moutend@gmail.com>
2017-11-18 10:40:37 +0900

    Update tests

--- timedivision_test.go
+++ timedivision_test.go
@@ -2,7 +2,7 @@ package midi
 
 import "testing"
 
-func TestSetBPM(t *testing.T) {
+func TestTimeDivision_SetBPM(t *testing.T) {
 	td := &TimeDivision{}
 
 	if err := td.SetBPM(480); err != nil {
@@ -13,5 +13,21 @@ func TestSetBPM(t *testing.T) {
 	}
 	if err := td.SetBPM(0x8001); err == nil {
 		t.Fatalf("err should not be nil")
+	}
+}
+
+func TestTimeDivision_Serialize(t *testing.T) {
+	td := &TimeDivision{value: 480}
+	expected := []byte{0x01, 0xe0}
+	actual := td.Serialize()
+
+	if len(expected) != len(actual) {
+		t.Fatalf("expected: %v actual: %v", len(expected), len(actual))
+	}
+	for i, e := range expected {
+		a := actual[i]
+		if e != a {
+			t.Fatalf("expected[%v] = %v actual[%v] = %v", i, e, i, a)
+		}
 	}
 }
