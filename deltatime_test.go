fd8c08c40529600fb0137545f2cb3573adbb077a
Yoshiyuki Koyanagi <moutend@gmail.com>
2017-11-27 08:59:52 +0900

    Update tests

--- deltatime_test.go
+++ deltatime_test.go
@@ -79,3 +79,18 @@ func TestParseDeltaTime(t *testing.T) {
 		}
 	}
 }
+
+func TestNewDeltaTime(t *testing.T) {
+	_, err := NewDeltaTime(0xffffffff)
+	if err == nil {
+		t.Fatalf("err must not be nil")
+	}
+
+	event, err := NewDeltaTime(0x0fffffff)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if event.Quantity().Uint32() != 0x0fffffff {
+		t.Fatalf("expected: 0x0fffffff actual: 0x%x", event.Quantity().Uint32())
+	}
+}
