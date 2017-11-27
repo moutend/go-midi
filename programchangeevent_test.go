e438ee69943d840e631827cc6a76b3ffbf571676
Yoshiyuki Koyanagi <moutend@gmail.com>
2017-11-27 09:03:15 +0900

    Update tests

--- programchangeevent_test.go
+++ programchangeevent_test.go
@@ -92,14 +92,24 @@ func TestProgramChangeEvent_Program(t *testing.T) {
 }
 
 func TestNewProgramChangeEvent(t *testing.T) {
-	event, err := NewProgramChangeEvent(nil, 1, 123)
+	_, err := NewProgramChangeEvent(nil, 255, 127)
+	if err == nil {
+		t.Fatalf("err must not be nil")
+	}
+
+	_, err = NewProgramChangeEvent(nil, 15, 255)
+	if err == nil {
+		t.Fatalf("err must not be nil")
+	}
+
+	event, err := NewProgramChangeEvent(nil, 15, 127)
 	if err != nil {
 		t.Fatal(err)
 	}
-	if event.channel != 1 {
-		t.Fatalf("expected: 1 actual: %v", event.channel)
+	if event.channel != 15 {
+		t.Fatalf("expected: 15 actual: %v", event.channel)
 	}
-	if event.program != 123 {
-		t.Fatalf("expected: 123 actual: %v", event.program)
+	if event.program != 127 {
+		t.Fatalf("expected: 127 actual: %v", event.program)
 	}
 }
