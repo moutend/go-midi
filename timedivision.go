75a8b3af096bb0d1e3cfcb4c748f5c19ad1d3538
Yoshiyuki Koyanagi <moutend@gmail.com>
2017-11-27 09:04:08 +0900

    Fix FPS and BPM methods

--- timedivision.go
+++ timedivision.go
@@ -2,42 +2,75 @@ package midi
 
 import "fmt"
 
-// TimeDivision represents time division for MIDI header.
+// TimeDivision represents the time division used to decode the delta time.
 type TimeDivision struct {
 	value uint16
 }
 
+// String returns string representation of time division.
 func (t *TimeDivision) String() string {
-	// assumes 120 bpm
 	if t.value == 0 {
-		return "default (120 bpm)"
+		return "&TimeDivision{bpm: 120}"
 	}
 	if t.value < 32768 {
-		return fmt.Sprintf("%d bpm", t.value)
+		return fmt.Sprintf("&TimeDivision{bpm: %d}", t.value)
 	}
-	fps := t.value & 0x7F00
+
+	frames := (t.value & 0x7F00) >> 8
 	ticks := t.value & 0x00FF
-	return fmt.Sprintf("%v ticks in %v fps", ticks, fps)
+
+	return fmt.Sprintf("&TimeDivision{frames: %v, ticks: %v}", frames, ticks)
 }
 
-func (t *TimeDivision) Set(rawdata uint16) {
-	t.value = rawdata
+// Serialize serializes time division.
+func (t *TimeDivision) Serialize() []byte {
+	if t.value == 0 {
+		return []byte{0x00, 0x78}
+	}
+
+	bs := make([]byte, 2)
+	bs[0] = byte(t.value >> 8)
+	bs[1] = byte(t.value & 0xff)
+
+	return bs
 }
 
+// SetBPM sets time division value as BPM.
 func (t *TimeDivision) SetBPM(bpm int) error {
 	if bpm >= 0x8000 {
-		return fmt.Errorf("midi: invalid BPM %d", bpm)
+		return fmt.Errorf("midi: BPM must be less than 32768")
 	}
 	t.value = uint16(bpm)
 
 	return nil
 }
 
-// Serialize serializes time division.
-func (t *TimeDivision) Serialize() []byte {
-	bs := make([]byte, 2)
-	bs[0] = byte(t.value >> 8)
-	bs[1] = byte(t.value & 0xff)
+// BPM returns time division as beat per minute.
+func (t *TimeDivision) BPM() (uint16, error) {
+	if t.value == 0 {
+		return 120, nil
+	}
+	if t.value < 0x8000 {
+		return t.value, nil
+	}
 
-	return bs
+	return 0, fmt.Errorf("midi: cannot retrieve value as BPM (%v)", t.value)
+}
+
+// SetFPS sets time division value as frames per second.
+func (t *TimeDivision) SetFPS(frames, ticks uint16) error {
+	t.value = 0x8000 + (frames << 8) + ticks
+
+	return nil
+}
+
+// FPS retuns time division as frames per second
+func (t *TimeDivision) FPS() (frames, ticks uint16, err error) {
+	if t.value <= 0x8000 {
+		return frames, ticks, fmt.Errorf("midi: cannot retrieve time division as FPS (%v)", t.value)
+	}
+	frames = (t.value >> 8) & 0x7f
+	ticks = t.value & 0xff
+
+	return frames, ticks, nil
 }
--- timedivision_test.go
+++ timedivision_test.go
@@ -2,23 +2,39 @@ package midi
 
 import "testing"
 
-func TestTimeDivision_SetBPM(t *testing.T) {
+func TestTimeDivision_String(t *testing.T) {
 	td := &TimeDivision{}
 
-	if err := td.SetBPM(480); err != nil {
-		t.Fatal(err)
+	expected := "&TimeDivision{bpm: 120}"
+	actual := td.String()
+
+	if expected != actual {
+		t.Fatalf("expected: %v actual: %v", expected, actual)
 	}
-	if err := td.SetBPM(0x8000); err == nil {
-		t.Fatalf("err should not be nil")
+
+	td = &TimeDivision{value: 32767}
+
+	expected = "&TimeDivision{bpm: 32767}"
+	actual = td.String()
+
+	if expected != actual {
+		t.Fatalf("expected: %v actual: %v", expected, actual)
 	}
-	if err := td.SetBPM(0x8001); err == nil {
-		t.Fatalf("err should not be nil")
+
+	td = &TimeDivision{value: 0x980a}
+
+	expected = "&TimeDivision{frames: 24, ticks: 10}"
+	actual = td.String()
+
+	if expected != actual {
+		t.Fatalf("expected: %v actual: %v", expected, actual)
 	}
 }
 
 func TestTimeDivision_Serialize(t *testing.T) {
-	td := &TimeDivision{value: 480}
-	expected := []byte{0x01, 0xe0}
+	td := &TimeDivision{}
+
+	expected := []byte{0x00, 0x78}
 	actual := td.Serialize()
 
 	if len(expected) != len(actual) {
@@ -29,5 +45,96 @@ func TestTimeDivision_Serialize(t *testing.T) {
 		if e != a {
 			t.Fatalf("expected[%v] = %v actual[%v] = %v", i, e, i, a)
 		}
+	}
+
+	td = &TimeDivision{value: 480}
+
+	expected = []byte{0x01, 0xe0}
+	actual = td.Serialize()
+
+	if len(expected) != len(actual) {
+		t.Fatalf("expected: %v actual: %v", len(expected), len(actual))
+	}
+	for i, e := range expected {
+		a := actual[i]
+		if e != a {
+			t.Fatalf("expected[%v] = %v actual[%v] = %v", i, e, i, a)
+		}
+	}
+}
+func TestTimeDivision_SetBPM(t *testing.T) {
+	td := &TimeDivision{}
+
+	if err := td.SetBPM(0x8000); err == nil {
+		t.Fatalf("err should not be nil")
+	}
+	if err := td.SetBPM(480); err != nil {
+		t.Fatal(err)
+	}
+
+	expected := uint16(480)
+	actual := td.value
+
+	if expected != actual {
+		t.Fatalf("expected: %v actual: %v", expected, actual)
+	}
+}
+
+func TestTimeDivision_BPM(t *testing.T) {
+	td := &TimeDivision{value: 32768}
+
+	_, err := td.BPM()
+	if err == nil {
+		t.Fatalf("err must not be nil")
+	}
+
+	td = &TimeDivision{}
+
+	expected := uint16(120)
+	actual, _ := td.BPM()
+
+	if expected != actual {
+		t.Fatalf("expected: %v actual: %v", expected, actual)
+	}
+
+	td = &TimeDivision{value: 32767}
+
+	expected = uint16(32767)
+	actual, _ = td.BPM()
+
+	if expected != actual {
+		t.Fatalf("expected: %v actual: %v", expected, actual)
+	}
+}
+
+func TestTimeDivision_SetFPS(t *testing.T) {
+	td := &TimeDivision{}
+	td.SetFPS(24, 10)
+
+	expected := uint16(0x980a)
+	actual := td.value
+
+	if expected != actual {
+		t.Fatalf("expected: %v actual: %v", expected, actual)
+	}
+}
+
+func TestTimeDivision_FPS(t *testing.T) {
+	td := &TimeDivision{}
+	_, _, err := td.FPS()
+	if err == nil {
+		t.Fatalf("err must not be nil")
+	}
+
+	td = &TimeDivision{value: 0x980a}
+
+	expectedFrames, expectedTicks := uint16(24), uint16(10)
+	actualFrames, actualTicks, _ := td.FPS()
+
+	if expectedFrames != actualFrames {
+		t.Fatalf("expected: %v actual: %v", expectedFrames, actualFrames)
+	}
+	if expectedTicks != actualTicks {
+		t.Fatalf("expected: %v actual: %v", expectedTicks, actualTicks)
 	}
 }
